1103261945327d8bbffe1405f12b8937a4af70fa
yingqiliu <f17qh@163.com>
2014-03-03 21:25:21 +0800

    update action

--- client/src/card_manager.cpp
+++ client/src/card_manager.cpp
@@ -10,45 +10,38 @@ void CardMgr::Init(int stage_id) {
   sprintf(name, "card_front_0%d.png", 1);
   int size = 3;
   for(int i = 1; i <= size; i++) {
-    if(i == 3) {
+    if(i == 3)
       sprintf(name, "card_back_0%d.png", 1);
-    }
-
     CCSprite* sprite = CCSprite::createWithSpriteFrameName(name);
     sprite->setPosition(ccp(152 + 243*(i-1), 229));
     card_layer_->addChild(sprite, 0, i-1);
     all_card_index_.push_back(i-1);
-    std::srand(time(0));
   }
+  std::srand(time(0));
+  CCNode::onEnter();
+  InitData();
 }
 
-void CardMgr::StartAction() {
-  std::vector<std::vector<int> > temp;
-  temp.push_back(all_card_index_);
+void CardMgr::InitData() {
+  card_lines_.push_back(all_card_index_);
   for(int times = 0; times < 10; ++times) {
     do {
       std::random_shuffle(all_card_index_.begin(), all_card_index_.end());
-    } while (temp[temp.size() -1] == all_card_index_);
-    temp.push_back(all_card_index_);
+    } while (card_lines_[card_lines_.size() -1] == all_card_index_);
+    card_lines_.push_back(all_card_index_);
   }
-  std::vector<CCFiniteTimeAction*> sprite_actions;
-  for(int sprite_num = 0; sprite_num < (int)all_card_index_.size(); sprite_num++) {
-    CCFiniteTimeAction* prve_action = NULL;
-    for(int random_time = 0; random_time < (int)temp.size() - 1; random_time++) {
-      prve_action = MovePosBy(prve_action, temp[random_time][sprite_num], temp[random_time+1][sprite_num], 186, 0.5f);
-      if(prve_action != NULL)
-        prve_action = CCSequence::createWithTwoActions(prve_action, CCDelayTime::create(0.3f));
-    }
-    if(prve_action != NULL) {
-      sprite_actions.push_back(prve_action);
-    }
+  run_time_ = 0;
+  action_nums_ = 0;
+}
+
+void CardMgr::StartAction() {
+  if(run_time_ >= int(card_lines_.size() - 1)) {
+    return;
   }
   for(int sprite_num = 0; sprite_num < (int)all_card_index_.size(); sprite_num++) {
-    if(sprite_actions[sprite_num] != NULL) {
-      CCSprite* sprite = (CCSprite*)card_layer_->getChildByTag(sprite_num);
-      sprite->runAction(sprite_actions[sprite_num]);
-    }
+    MovePosBy(card_lines_[run_time_][sprite_num], card_lines_[run_time_+1][sprite_num], 186, 0.5f);
   }
+  run_time_++;
 }
 
 CCLayer* CardMgr::card_layer() {
@@ -56,33 +49,25 @@ CCLayer* CardMgr::card_layer() {
   return card_layer_;
 }
 
-CCFiniteTimeAction* CardMgr::MovePosBy(CCFiniteTimeAction* prve_action, int source_index, int target_index, int high, float time) {
-  //CCSprite* src = (CCSprite*)card_layer_->getChildByTag(source_index);
+void CardMgr::MovePosBy(int source_index, int target_index, int high, float time) {
+  CCSprite* src = (CCSprite*)card_layer_->getChildByTag(source_index);
   CCPoint start_pos = card_layer_->getChildByTag(source_index)->getPosition();
   CCPoint end_pos = card_layer_->getChildByTag(target_index)->getPosition();
-  //int dalt = source_index - target_index;
   int dalt = (start_pos.x - end_pos.x) / 243.0f;
   if(dalt < 0) {
-    if(dalt == -1) {
-      return MoveWithLine(prve_action, end_pos, time);
-    } else {
-      return MoveWithBezier(prve_action, start_pos, end_pos, high, time);
-    }
+    MoveWithBezier(src, start_pos, end_pos, high, time);
   } else if(dalt > 0) {
     if(dalt == 1) {
-      return MoveWithLine(prve_action, end_pos, time);
+      MoveWithLine(src, end_pos, time);
     } else {
-      return MoveWithBezier(prve_action, start_pos, end_pos, -high, time);
+      MoveWithBezier(src, start_pos, end_pos, -high, time);
     }
-  } else {
-    return prve_action;
   }
-  
 }
 
-CCFiniteTimeAction* CardMgr::MoveWithBezier(CCFiniteTimeAction* prve_action, CCPoint start_point, CCPoint end_point, int high, float time) {
-  //CCAssert(prve_action != NULL, "prve_action is null");
-  //src->setPosition(start_point);
+void CardMgr::MoveWithBezier(CCSprite* src, CCPoint start_point, CCPoint end_point, int high, float time) {
+  CCAssert(src != NULL, "src is null");
+  src->setPosition(start_point);
   float sx = start_point.x;
   float sy = start_point.y;
   float ex = end_point.x;
@@ -93,22 +78,26 @@ CCFiniteTimeAction* CardMgr::MoveWithBezier(CCFiniteTimeAction* prve_action, CCP
   bezier.controlPoint_2 = ccp(ex, ey+high); //控制点
   bezier.endPosition = ccp(ex, ey); // 结束位置
   CCBezierTo* cur_action = CCBezierTo::create(time, bezier);
-  if(prve_action == NULL) {
-    return cur_action;
-  } else {
-    CCSequence* res = CCSequence::createWithTwoActions(prve_action, cur_action);
-    return res;
-  }
-  //src->runAction(action_move);
+  CCCallFuncN* end = CCCallFuncN::create(this, callfuncN_selector(CardMgr::MoveEnd));
+  CCSequence* res = CCSequence::createWithTwoActions(cur_action, end);
+  action_nums_++;
+  src->runAction(res);
 }
 
-CCFiniteTimeAction* CardMgr::MoveWithLine(CCFiniteTimeAction* prve_action, CCPoint end_point, float time) {
+void CardMgr::MoveWithLine(CCSprite* src, CCPoint end_point, float time) {
   CCMoveTo* cur_action = CCMoveTo::create(time, end_point);
-  if(prve_action == NULL) {
-    return cur_action;
-  } else {
-    CCSequence* res = CCSequence::createWithTwoActions(prve_action, cur_action);
-    return res;
-  }
-  //src->runAction(action_move);
+  CCCallFuncN* end = CCCallFuncN::create(this, callfuncN_selector(CardMgr::MoveEnd));
+  CCSequence* res = CCSequence::createWithTwoActions(cur_action, end);
+  action_nums_++;
+  src->runAction(res);
 }
+
+void CardMgr::OnTime(float f) {
+   StartAction();
+}
+void CardMgr::MoveEnd(CCNode* sender) {
+  action_nums_--;
+  if(action_nums_ == 0)
+    scheduleOnce(schedule_selector(CardMgr::OnTime), 0.3f);
+}
+
--- client/src/card_manager.h
+++ client/src/card_manager.h
@@ -4,18 +4,24 @@
 
 USING_NS_CC;
 
-class CardMgr {
+class CardMgr : public CCNode {
 
 public:
   void Init(int stage_id);
   void StartAction();
   CCLayer* card_layer();
 protected:
-  CCFiniteTimeAction* MovePosBy(CCFiniteTimeAction* prve_action, int source_index, int target_index, int high, float time);
-  CCFiniteTimeAction* MoveWithBezier(CCFiniteTimeAction* prve_action, CCPoint start_point, CCPoint end_point, int high, float time);
-  CCFiniteTimeAction* MoveWithLine(CCFiniteTimeAction* prve_action, CCPoint end_point, float time);
+  void MovePosBy(int source_index, int target_index, int high, float time);
+  void MoveWithBezier(CCSprite* src, CCPoint start_point, CCPoint end_point, int high, float time);
+  void MoveWithLine(CCSprite* src, CCPoint end_point, float time);
+  void MoveEnd(CCNode* sender);
+  void OnTime(float);
+  void InitData();
 protected:
   CCLayer *card_layer_;
   int bingo_index_;
+  int run_time_;
+  int action_nums_;
   std::vector<int> all_card_index_;
+  std::vector<std::vector<int> > card_lines_;
 };
